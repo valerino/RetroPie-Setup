72f22a50eca21e1a0fb711091c881eac1442feb4
valerino <xoanino@gmail.com>
2020-04-26 11:10:33 +0100

    added lr-mess-electron.sh (Acorn Electron), same issue as bbcmicro so also this bypass runcommand.sh. i may think to better solution later.....

---
+++ scriptmodules/libretrocores/lr-mess-electron.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="lr-mess-electron"
+rp_module_name="Acorn Electron"
+rp_module_ext=".zip .wav .csw .uef .bin"
+rp_module_desc="MESS emulator ($rp_module_name) - MESS Port for libretro"
+rp_module_help="ROM Extensions: $rp_module_ext\n\n
+Put games in:\n
+$romdir/electron\n\n
+Put BIOS files in $biosdir:\n
+electron.zip\n\n
+How to load games:\n
+*TAPE<enter>\n
+CHAIN\"\"<enter>\n"
+
+rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/mame/master/LICENSE.md"
+rp_module_section="exp"
+rp_module_flags=""
+
+function depends_lr-mess-electron() {
+	local _mess=$(dirname "$md_inst")/lr-mess/mess_libretro.so
+	if [[ ! -f "$_mess" ]]; then
+		printMsgs dialog "cannot find '$_mess' !\n\nplease install 'lr-mess' package."
+		exit 1
+	fi
+}
+
+function sources_lr-mess-electron() {
+	true
+}
+
+function build_lr-mess-electron() {
+	true
+}
+
+function install_lr-mess-electron()  {
+	true
+}
+
+function configure_lr-mess-electron() {
+	local _mess=$(dirname "$md_inst")/lr-mess/mess_libretro.so
+	local _retroarch_bin="$rootdir/emulators/retroarch/bin/retroarch"
+	local _system="electron"
+	local _config="$configdir/$_system/retroarch.cfg"
+	local _add_config="$_config.add"
+	local _custom_coreconfig="$configdir/$_system/custom-core-options.cfg"
+	local _script="$configdir/$_system/run_mess.sh"
+
+	# create retroarch configuration
+	ensureSystemretroconfig "$_system"
+
+	# ensure it works without softlists, using a custom per-fake-core config
+    iniConfig " = " "\"" "$_custom_coreconfig"
+    iniSet "mame_softlists_enable" "disabled"
+	iniSet "mame_softlists_auto_media" "disabled"
+	iniSet "mame_boot_from_cli" "disabled"
+
+	# this will get loaded too via --append_config
+	iniConfig " = " "\"" "$_add_config"
+	iniSet "core_options_path" "$_custom_coreconfig"
+	#iniSet "save_on_exit" "false"
+
+	# setup rom folder
+	mkRomDir "$_system"
+
+	# copy the juicy script which will do the all the hard work to the fake-core config folder
+	cp "$scriptdir/scriptmodules/run_mess.sh" "$_script"
+	chmod 755 "$_script"
+
+	# add the emulators.cfg as normal, pointing to the above script
+	addEmulator 1 "$md_id-cass" "$_system" "$_script $_retroarch_bin $_mess $_config electron $biosdir -cass %ROM%"
+	#addEmulator 0 "$md_id-cart" "$_system" "$_script $_retroarch_bin $_mess $_config electron $biosdir -cart %ROM%"
+
+	# add system to es_systems.cfg as normal
+	addSystem "$_system" "$md_name" "$md_ext"
+
+	# FIXME: this is a dirty hack until we find why this emulator doesn't work through runcommand.sh
+	# this overwrites the entry in es_system.cfg to call run_mess.sh directly without passing by runcommand.sh
+	setESSystem "$md_name" "$_system" "$romdir/$_system" "$md_ext" "$_script $_retroarch_bin $_mess $_config electron $biosdir -cass %ROM%" "$_system" "$_system"
+
+}
