2bf05dfada0d7d0edfaf443a4fd6b4b0f9db9467
valerino <xoanino@gmail.com>
2020-04-30 01:32:41 +0100

    changed lr-beetle-supergrafx roms folder and identifier to "sgfx" to match with most themes

--- scriptmodules/libretrocores/lr-beetle-supergrafx.sh
+++ scriptmodules/libretrocores/lr-beetle-supergrafx.sh
@@ -11,7 +11,7 @@
 
 rp_module_id="lr-beetle-supergrafx"
 rp_module_desc="SuperGrafx TG-16 emulator - Mednafen PCE Fast port for libretro"
-rp_module_help="ROM Extensions: .pce .ccd .cue .zip\n\nCopy your PC Engine / TurboGrafx roms to $romdir/pcengine\n\nCopy the required BIOS file syscard3.pce to $biosdir"
+rp_module_help="ROM Extensions: .pce .ccd .cue .zip\n\nCopy your PC Engine SuperGrafx roms to $romdir/pcengine\n\nCopy the required BIOS file syscard3.pce to $biosdir"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/beetle-supergrafx-libretro/master/COPYING"
 rp_module_section="main"
 
@@ -32,9 +32,9 @@ function install_lr-beetle-supergrafx() {
 }
 
 function configure_lr-beetle-supergrafx() {
-    mkRomDir "pcengine"
-    ensureSystemretroconfig "pcengine"
+    mkRomDir "sgfx"
+    ensureSystemretroconfig "sgfx"
 
-    addEmulator 0 "$md_id" "pcengine" "$md_inst/mednafen_supergrafx_libretro.so"
-    addSystem "pcengine"
+    addEmulator 0 "$md_id" "sgfx" "$md_inst/mednafen_supergrafx_libretro.so"
+    addSystem "sgfx" "Supergrafx" ".pce .zip"
 }
