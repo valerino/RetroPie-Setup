8699f274ea549f9b4ed229acc696a78dbbfad136
valerino <xoanino@gmail.com>
2020-05-31 21:15:20 +0100

    bumped retroarch to 1.8.8

--- scriptmodules/emulators/retroarch.sh
+++ scriptmodules/emulators/retroarch.sh
@@ -39,10 +39,10 @@ function depends_retroarch() {
 }
 
 function sources_retroarch() {
-    gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.8.6
-    applyPatch "$md_data/01_hotkey_hack.diff"
-    applyPatch "$md_data/02_disable_search.diff"
-    applyPatch "$md_data/03_shader_path_config_enable.diff"
+    gitPullOrClone "$md_build" https://github.com/libretro/RetroArch.git v1.8.8
+    #applyPatch "$md_data/01_hotkey_hack.diff"
+    #applyPatch "$md_data/02_disable_search.diff"
+    #applyPatch "$md_data/03_shader_path_config_enable.diff"
 }
 
 function build_retroarch() {
